663486a132081c811cae7d5c8ce0127233130d64
Jan Grodowski <jgrodowski@gmail.com>
2014-12-02 10:41:13 +0100

    merging solved

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,3 +1,36 @@
-class Diamond 
+class Diamond
+  class << self
+    def print(letter)
+      res = []
+      with_alphabet_for(letter) do |l|
+        res << row(l, letter)
+      end
+      (res + res[0..-2].reverse).join('')
+    end
+    
+    private
+    
+    def row(l, letter)
+      indent(l, letter) + \
+      l + spacing(l) + \
+      (l == 'A' ? '' : l) + "\n"
+    end
+    
+    def indent(l, letter)
+      ' ' * (letter.ord - l.ord)
+    end
+    
+    def spacing(l)
+      return '' if l == 'A'
+      ' ' * (2 * (l.ord - 'A'.ord) - 1)
+    end
   
+    def with_alphabet_for(letter)
+      res = []
+      letters = Array('A'..letter)
+      letters.each do |l|
+        yield l
+      end
+    end
+  end
 end
--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -1,5 +1,16 @@
 require 'diamond'
 
 describe Diamond do 
+  it 'prints A for input A' do
+    expect(Diamond.print('A')).to eq "A\n"
+  end
   
+  it 'prints diamond for B' do 
+    expect(Diamond.print('B')).to eq " A\nB B\n A\n"
+  end
+  
+  it 'prints diamond for C' do 
+    expected = "  A\n B B\nC   C\n B B\n  A\n"
+    expect(Diamond.print('C')).to eq expected
+  end
 end
